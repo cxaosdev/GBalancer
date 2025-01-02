b9fc7fdf5cee3c2f2a13d78a177bd35f57a0e1b3
Chaeyoung Shin <scy0723123@gmail.com>
2025-01-02 15:35:10 +0900

    Feat: 푸터 아이콘 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
-import { FaGithub, FaPhoneAlt } from "react-icons/fa";
+import { LiaGithub } from "react-icons/lia";
+import { VscFeedback } from "react-icons/vsc";
 import { useNavigate } from "react-router-dom";
 
 export default function Footer() {
@@ -28,21 +29,19 @@ export default function Footer() {
                 </li>
               </ul>
             </nav>
-            <div className="fixed bottom-0 right-[2rem] flex items-end text-[1rem]">
+            <div className="fixed bottom-0 right-[1rem] flex items-end text-[1rem]">
               <div className="flex items-center text-white/80">
-                <FaPhoneAlt />
-                <span className="text-white/80">
-                  &nbsp;Contact: scy0723123@gmail.com &nbsp;
-                </span>
+                <VscFeedback />
+                <span className="ml-1 text-white/80">scy0723123@gmail.com</span>
               </div>
               <a
                 href="https://github.com/cxaosdev"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <div className="flex items-center mt-1 text-white/80">
-                  <FaGithub className="" />
-                  <span className="ml-2 cursor-pointer text-white/80 hover:text-amber-600">
+                <div className="flex items-center mt-1 ml-1 text-white/80">
+                  <LiaGithub className="text-[1.2rem]" />
+                  <span className="ml-1 cursor-pointer text-white/80 hover:text-amber-600">
                     cxaosdev
                   </span>
                 </div>
